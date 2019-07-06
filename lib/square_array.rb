8dd6af95a78cd87c8edfb47e4ce1f76641e0f564
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-06 16:29:42 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  count = 0
+  new_square_array = []
+  while array[count] do
+    new_square_array << array[count].to_i ** 2
+    count += 1
+  end
+  new_square_array
 end
+
+# takes an array argument and then squares each element
+# and returns the new squared array
+# while loop
